20dac8df163e9af13a112dba4c920fb30dc749cf
ksdumont <ksdumont@gmail.com>
2019-12-15 10:58:11 -0800

    added helmet and cors

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const POKEDEX = require('./pokedex.json');
+const helmet = require('helmet');
+const cors = require('cors');
+
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
 
 app.use(function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization');
